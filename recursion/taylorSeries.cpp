d096fad085ed3334f1433fdabf669b94c1bf2f32
Yash Kale <yashkale46@gmail.com>
2022-03-23 22:07:47 +0530

    Taylor Series

--- recursion/taylorSeries.cpp
+++ recursion/taylorSeries.cpp
@@ -6,9 +6,17 @@ double e(int x, int n)
 {
     static double p = 1, f = 1;
     double rv;
+    if (n == 0)
+        return 1;
+    rv = e(x, n - 1);
+    p = p * x;
+    f = f * n;
+    return rv + (p / f);
 }
 
 int main()
 {
+    double rv = e(4, 100);
+    printf("%lf \n", rv);
     return 0;
 }
