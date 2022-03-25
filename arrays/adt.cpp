0141a4eec8d08784eddd49a73b1e971ab662ff0e
Yash Kale <yashkale46@gmail.com>
2022-03-25 09:55:30 +0530

    Array ADT : LinearSearch ImprovedTranspostionLinearSearch ImprovedMoveToFrontLinearSearch

--- arrays/adt.cpp
+++ arrays/adt.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
-
+using namespace std;
 struct Array
 {
     int A[6];
@@ -42,15 +42,65 @@ void Delete(struct Array *r, int index)
     };
 };
 
+int LinearSearch(struct Array r, int key)
+{
+    for (int currIndex = 0; currIndex < r.length; currIndex++)
+    {
+        if (key == r.A[currIndex])
+        {
+            printf("Key found at index %d ", currIndex);
+            return currIndex;
+        }
+    }
+    return -1;
+}
+
+void swap(int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+// Transpostion
+int ImprovedTranspostionLinearSearch(struct Array *r, int key)
+{
+    for (int currIndex = 0; currIndex < r->length; currIndex++)
+    {
+        if (key == r->A[currIndex])
+        {
+            swap(&r->A[currIndex], &r->A[currIndex - 1]);
+            return currIndex;
+        };
+    }
+    return -1;
+};
+
+int ImprovedMoveToFrontLinearSearch(struct Array *r, int key)
+{
+    for (int currIndex = 0; currIndex < r->length; currIndex++)
+    {
+        if (key == r->A[currIndex])
+        {
+            swap(&r->A[currIndex], &r->A[0]);
+            return currIndex;
+        };
+    }
+    return -1;
+};
+
 int main()
 {
     struct Array r = {{4, 5, 6, 3}, 6, 4};
     Traverse(r);
 
+    Traverse(r);
     // Insert(&r, 2, 100);
     // Append(&r, 10);
-    Traverse(r);
-    Delete(&r, 1);
+    // Delete(&r, 1);
+    int searchResult = ImprovedMoveToFrontLinearSearch(&r, 3);
+    cout << "Result " << searchResult;
     Traverse(r);
     return 0;
 }
